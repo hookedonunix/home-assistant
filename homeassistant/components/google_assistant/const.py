eec3008445426c2659d17f4474daec9720334116
hookedonunix <hookedonunix@protonmail.com>
2022-05-22 23:52:04 +0000

    Add WATERHEATER device type to google assistant

--- homeassistant/components/google_assistant/const.py
+++ homeassistant/components/google_assistant/const.py
@@ -21,6 +21,7 @@ from homeassistant.components import (
     sensor,
     switch,
     vacuum,
+    water_heater,
 )
 
 DOMAIN = "google_assistant"
@@ -87,6 +88,7 @@ TYPE_SWITCH = f"{PREFIX_TYPES}SWITCH"
 TYPE_THERMOSTAT = f"{PREFIX_TYPES}THERMOSTAT"
 TYPE_TV = f"{PREFIX_TYPES}TV"
 TYPE_VACUUM = f"{PREFIX_TYPES}VACUUM"
+TYPE_WATERHEATER = f"{PREFIX_TYPES}WATERHEATER"
 
 SERVICE_REQUEST_SYNC = "request_sync"
 HOMEGRAPH_URL = "https://homegraph.googleapis.com/"
@@ -141,6 +143,7 @@ DOMAIN_TO_GOOGLE_TYPES = {
     sensor.DOMAIN: TYPE_SENSOR,
     switch.DOMAIN: TYPE_SWITCH,
     vacuum.DOMAIN: TYPE_VACUUM,
+    water_heater.DOMAIN: TYPE_WATERHEATER,
 }
 
 DEVICE_CLASS_TO_GOOGLE_TYPES = {
